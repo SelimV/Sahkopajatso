45dc4126c124de5e4d0464c806ecbf0bcb855dd6
SelimV <selim.virtanen@gmail.com>
2020-06-01 03:00:04 +0300

    the app is pretty much ready

--- sahkopajatso_app/lib/main.dart
+++ sahkopajatso_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:math';
 import 'dart:ffi';
+import 'package:toast/toast.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Sähköpajatso'),
     );
   }
 }
@@ -64,6 +66,39 @@ class _MyHomePageState extends State<MyHomePage> {
   BluetoothDevice _connectedDevice;
   List<BluetoothService> _services;
 
+  //state values for scorekeeping
+  int _score = 0;
+  int _shotsFired = 0;
+  List<int> _previousData = [0, 0, 0, 0];
+  List<int> _weights = [1, 1, 1, 1];
+  static int availableWeights = 9;
+
+  //takes a bluetooth result, decodes it and updates the score
+  void fetchResults(List<int> value) {
+    //decode the value
+    String newString = utf8.decode(value);
+
+    //check that there is a updated score
+    if (newString.startsWith('[')) {
+      //parse the string into a list of integers
+      List<int> newData = newString
+          .replaceAll('[', '')
+          .replaceAll(']', '')
+          .split(',')
+          .map((e) => int.tryParse(e))
+          .toList();
+      setState(() {
+        //if new hits have been made, increase the score according to the weigths
+        for (var i = 0; i < newData.length; i++) {
+          _score += (newData[i] - _previousData[i]) * _weights[i] * 1000;
+        }
+        _previousData = newData;
+      });
+    } else {
+      print(newString);
+    }
+  }
+
   //function that selects a device and stops scanning
   _setDevice(BluetoothDevice device) async {
     widget.flutterBlue.stopScan();
@@ -76,6 +111,9 @@ class _MyHomePageState extends State<MyHomePage> {
     } finally {
       //get the services of the connected device
       _services = await device.discoverServices();
+      await _services.last.characteristics.first
+          .setNotifyValue(true); //Subscribe to the score service
+      _services.last.characteristics.first.value.listen(fetchResults);
     }
     setState(() {
       _connectedDevice = device;
@@ -132,10 +170,111 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 50,
             ))
         .toList();
-
     return ListView(
       padding: EdgeInsets.all(8),
       children: <Widget>[...rows],
+    );
+  }
+
+  //with these functions the user can adjust the weights for scoring
+  void increaseWeight(int i) {
+    int remainingWeight = availableWeights;
+    for (var weight in _weights) {
+      remainingWeight -= weight;
+    }
+    if (remainingWeight > 0) {
+      setState(() {
+        _weights[i]++;
+      });
+    } else {
+      Toast.show("All weights in use", context);
+    }
+  }
+
+  void decreaseWeight(int i) {
+    if (_weights[i] > 0) {
+      setState(() {
+        _weights[i]--;
+      });
+    } else {
+      Toast.show("Weights must be non-negative", context);
+    }
+  }
+
+  Widget _buildWeightsRow() {
+    List<Widget> weightWidgets = _weights.asMap().entries.map((entry) {
+      int e = entry.value;
+      int i = entry.key;
+      return Expanded(
+        flex: 1,
+        child: Center(
+          child: Column(
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_drop_up),
+                onPressed: () => increaseWeight(i),
+              ),
+              Center(
+                child: Text(
+                  '$e',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_drop_down),
+                onPressed: () => decreaseWeight(i),
+              ),
+            ],
+          ),
+        ),
+      );
+    }).toList();
+
+    int remainingWeight = availableWeights;
+    for (var weight in _weights) {
+      remainingWeight -= weight;
+    }
+
+    Widget remaining = Center(
+      child: Column(
+        children: [
+          Text(
+            'Remaining',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          Container(
+            height: 12,
+          ),
+          Text(
+            '$remainingWeight',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+    );
+
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: weightWidgets,
+              ),
+            ),
+          ),
+          Expanded(flex: 1, child: remaining),
+        ],
+      ),
     );
   }
 
@@ -158,7 +297,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ))
         .toList();
 
-    //builds a view that lets user interact with the connected devicew and shows the available bluetooth services
+    //builds a view that lets user interact with the connected device and shows the available bluetooth services
     return Column(
       children: [
         Container(
@@ -167,37 +306,94 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               Expanded(
                 child: Center(
-                  child: Text(_connectedDevice.name == "" //adds a placeholder for empty names
+                  child: Text(_connectedDevice.name ==
+                          "" //adds a placeholder for empty names
                       ? "???"
                       : _connectedDevice.name),
                 ),
               ),
-              FlatButton(
-                onPressed: () async{await _services.last.characteristics.first.write(utf8.encode('1'));}, //sends the string '1' to the BLE-module when the button is pressed
-                child: Text(
-                  "ON",
-                  style: Theme.of(context).accentTextTheme.button,
-                ),
-                color: Theme.of(context).accentColor,
-              ),
-              Container(width:8),
-              FlatButton(
-                onPressed: () async{await _services.last.characteristics.first.write(utf8.encode('0'));}, //sends the string '0' to the BLE-module when the button is pressed
-                child: Text(
-                  "OFF",
-                  style: Theme.of(context).accentTextTheme.button,
-                ),
-                color: Theme.of(context).accentColor,
-                
-              ),
             ],
           ),
         ),
-        Expanded(
-          child: ListView(
-            padding: EdgeInsets.all(8),
-            children: <Widget>[...rows],
+        Text(
+          'Score',
+          style: Theme.of(context).textTheme.headline2,
+        ),
+        Text(
+          '$_score',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Shots fired',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                      '$_shotsFired',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Score/Shot',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                      '${(_score / max(_shotsFired, 1)).floor()}',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        FlatButton(
+          onPressed: () {
+            setState(() {
+              _score = 0;
+              _shotsFired = 0;
+            });
+          }, //resets the score
+          child: Text(
+            "Restart",
+            style: Theme.of(context).accentTextTheme.button,
           ),
+          color: Theme.of(context).accentColor,
+        ),
+        Text(
+          'Weights',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        _buildWeightsRow(),
+        RaisedButton(
+          onPressed: () async {
+            setState(() {
+              _shotsFired++;
+            });
+            await _services.last.characteristics.first.write(utf8.encode('s'));
+          }, //sends the string 's' (shoot) to the BLE-module when the button is pressed
+          child: Text(
+            "Shoot",
+            style: TextStyle(
+                fontSize: 32.0, textBaseline: TextBaseline.alphabetic),
+          ),
+          color: Theme.of(context).accentColor,
+          textColor: Theme.of(context).accentTextTheme.button.color,
+          padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
         ),
       ],
     );
@@ -260,23 +456,11 @@ class _MyHomePageState extends State<MyHomePage> {
             // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.display1,
-              ),
               Expanded(child: _buildBody()),
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
